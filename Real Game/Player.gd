a737d525e0402d3043da0e6e6f815d6fb3962e3e
Necradow <52135643+Necradow@users.noreply.github.com>
2019-06-25 19:31:11 +0200

    Add files via upload

--- Real Game/Player.gd
+++ Real Game/Player.gd
@@ -1,21 +1,39 @@
 extends Area2D
 	
 export var speed = 400
+
 var screen_size
 
 func _ready():
 	screen_size = get_viewport_rect().size
-
+	
+func _input(event):
+	
+	if event is InputEventKey:
+		#sprint command
+		if event.scancode == KEY_SHIFT: #key to sprint
+			speed = 800 #sprinting movement speed
+		else:
+			speed = 400 #normal movement speed
+		
+		
 func _process(delta):
+	#2-element structure that can be used to represent positions in 2d space or any other pair of numeric values.
 	var velocity = Vector2()
+	
+	#move right
 	if Input.is_action_pressed("ui_right"):
 		velocity.x += 1
+	#move left
 	if Input.is_action_pressed("ui_left"):
 		velocity.x -= 1
+	#move down
 	if Input.is_action_pressed("ui_down"):
 		velocity.y += 1
+	#move up
 	if Input.is_action_pressed("ui_up"):
 		velocity.y -= 1
+		
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * speed
 		$AnimatedSprite.play()
@@ -33,6 +51,3 @@ func _process(delta):
 	elif velocity.y != 0:
 		$AnimatedSprite.animation = "up"
 		$AnimatedSprite.flip_v = velocity.y > 0
-		
-	#test
-		
